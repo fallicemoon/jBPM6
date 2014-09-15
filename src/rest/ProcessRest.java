a40363a7927b336f5188b140531ed081c0fe3a38
fallicemoon <Henry.H.Wu@newegg.com>
2014-09-15 12:02:47 +0800

    Revert "remove unused"
    This reverts commit 32d821025c18c8810a171551290cc5588421d174.

---
+++ src/rest/ProcessRest.java
@@ -0,0 +1,159 @@
+package rest;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ejb.EJB;
+import javax.script.ScriptException;
+import javax.ws.rs.*;
+
+import org.jboss.logging.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import utility.RemoteRest;
+
+@Path("/process")
+public class ProcessRest extends Rest{
+
+	public ProcessRest() {
+		
+	}
+	
+	//REST
+	@POST
+	public String createLeave(String jsonString) throws ScriptException{
+		String reason;
+		Integer days;
+
+		try {
+			JSONObject json = new JSONObject(jsonString);
+			reason = json.getString("reason");
+			days = json.getInt("days");
+			
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("map_reason", reason);
+			map.put("map_days", days.toString());
+			
+			RemoteRest remoteRest = new RemoteRest();
+			String processInstanceId = remoteRest.createProcessInstance("com.newegg.henry:henry_proj:1.0", "henry_project.leave_request", map);
+			Map<String, Object> responseMap = new HashMap<String, Object>();
+			map.put("processInstanceId", processInstanceId.toString());
+			
+			return getSuccessResponseJson(responseMap);
+			
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return getFailResponseJson();
+			
+		} catch (IOException e) {
+			logger.info("http return BufferReader has some problem......");
+			e.printStackTrace();
+			return getFailResponseJson();	
+		}				
+	}
+	
+	@GET
+	public String getTaskOfLeaveList(String jsonString){
+		RemoteRest remoteRest = new RemoteRest();
+		String taskName;
+		
+		try {
+			JSONObject json = new JSONObject(jsonString);
+			taskName = json.getString("taskName");
+			JSONObject responseJson = remoteRest.getAllTasksByTaskName("com.henry:henry_proj:1.0", "henry_project.leave_request",taskName);
+			return responseJson.toString();
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return getFailResponseJson();
+		} 
+		
+	}
+	
+	@GET
+	public String getLeave(String jsonString){
+		RemoteRest remoteRest = new RemoteRest();
+		String processInstanceId;
+		
+		try {
+			JSONObject json = new JSONObject(jsonString);
+			processInstanceId = json.getString("processInstanceId");
+			JSONObject responseJson = remoteRest.getProcessInstanceVar(processInstanceId);
+			return responseJson.toString();
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return getFailResponseJson();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return getFailResponseJson();
+		} 
+		
+	}
+	
+	@GET
+	public String getLeaveTask(String jsonString){
+		RemoteRest remoteRest = new RemoteRest();
+		String processInstanceId;
+		
+		try {
+			JSONObject json = new JSONObject(jsonString);
+			processInstanceId = json.getString("processInstanceId");
+			JSONObject responseJson = remoteRest.getProcessInstanceVar(processInstanceId);
+			return responseJson.toString();
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return getFailResponseJson();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return getFailResponseJson();
+		} 
+		
+	}
+	
+	
+	
+	@PUT
+	public String assignLeave(String jsonString){
+		RemoteRest remoteRest = new RemoteRest();
+		String taskId;
+		
+		try {
+			JSONObject json = new JSONObject(jsonString);
+			taskId = json.getString("taskId");
+			JSONObject responseJson = remoteRest.getAllTasksByTaskName("com.henry:henry_project:1.0", "henry_project.leave_request",taskId);
+			return responseJson.toString();
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return getFailResponseJson();
+		} 
+		
+	}
+	
+	@PUT
+	public String approveLeave(String jsonString){
+		RemoteRest remoteRest = new RemoteRest();
+		String isApproved;
+		String taskId;
+		
+		try {
+			JSONObject json = new JSONObject(jsonString);
+			isApproved = String.valueOf(json.getBoolean("isApproved"));
+			taskId = json.getString("taskId");
+			
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("map_isApproved", isApproved);
+			remoteRest.completeTask(taskId, map);
+			
+			return getSuccessResponseJson(new HashMap<String, Object>());
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return getFailResponseJson();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return getFailResponseJson();
+		}
+		
+	}
+
+}
---
+++ src/rest/Rest.java
@@ -0,0 +1,50 @@
+package rest;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+import org.jboss.logging.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import utility.RemoteRest;
+
+@Path("/login")
+public class Rest {
+	protected Logger logger = Logger.getLogger(this.getClass());
+	
+
+	public Rest() {
+		
+	}
+	
+	protected String getSuccessResponseJson(Map<String, Object> responseMap) {
+		JSONObject responseJsonObject = new JSONObject(responseMap);
+		try {
+			responseJsonObject.put("success", "true");
+			return responseJsonObject.toString();
+			
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return "{\"success\":\"false\"}";
+		}
+		
+	}
+	
+	protected String getFailResponseJson() {
+		JSONObject responseJsonObject = new JSONObject();
+		try {
+			responseJsonObject.put("success", "false");
+			return responseJsonObject.toString();
+			
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return "{\"success\":\"false\"}";
+		}
+	}
+
+
+}
